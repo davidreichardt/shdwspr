d812a4cc2623f5d447da917dcca9bb7c4565e256
David Reichardt <davereichardt@gmail.com>
2025-06-01 17:54:24 -0400

    update user model and ranks

---
+++ prisma/migrations/20250601215347_update_user_model_and_ranks/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - Added the required column `discordHandle` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "rsiHandle" TEXT,
+    "discordId" TEXT NOT NULL,
+    "discordHandle" TEXT NOT NULL,
+    "preferredName" TEXT,
+    "rank" TEXT NOT NULL DEFAULT 'RECRUIT',
+    "avatar" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("avatar", "createdAt", "discordId", "id") SELECT "avatar", "createdAt", "discordId", "id" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_rsiHandle_key" ON "User"("rsiHandle");
+CREATE UNIQUE INDEX "User_discordId_key" ON "User"("discordId");
+CREATE UNIQUE INDEX "User_discordHandle_key" ON "User"("discordHandle");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
